edbcaad16c4de966453c46b173171348c9785c0a
Sharad Duwal <sharad.duwal@gmail.com>
2025-03-19 09:03:37 +0545

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ prompt_models = ["CohereForAI/aya-expanse-8b", "google/gemma-2-9b-it", "Qwen/Qwe
 similarity_model = 'paraphrase-multilingual-mpnet-base-v2'
 answer_similarity_threshold = 0.85
 
+# prompt_instruction = "Given below is a question, possible choices and the correct answer.\n\n"
+
 prompt_instruction = """You are an expert assessment system. Analyze the question below, evaluate all choices, and select the most accurate answer.
 Respond ONLY with the correct answer, no additional info required.\n\n"""
 
